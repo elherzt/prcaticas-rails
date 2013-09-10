c99873c868b875fc37bf28ed5091c3e58fe0b4cc
Jesus Romero <jose.romero@tangosource.com>
2013-09-10 18:16:00 -0500

    created migration

---
+++ db/migrate/20130910230835_create_table_states.rb
@@ -0,0 +1,12 @@
+class CreateTableStates < ActiveRecord::Migration
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.string :color
+      t.string :background
+    end
+    add_column :cars, :state_id, :integer
+    add_index :cars, :state_id
+    add_column :comments, :state_id, :integer
+  end
+end
