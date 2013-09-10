ff73fe26e038915497f82798ea8227c26290ef0c
Jesus Romero <jose.romero@tangosource.com>
2013-09-10 16:51:39 -0500

    added user and car factories

--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 feature "Creating comments" do
-  let!(:user) { Factory(:confirmed_user) }
+  let!(:user) { Factory(:user) }
   let!(:car) { Factory(:car) }
+
+
+ before do
+   user = factorygirl.create(:user) 
+   car = factorygirl.create(:car)
+    visit '/'
+    click_link project.name
+    click_link "new ticket" 
+    fill_in "title", with: "add documentation for blink tag"
+    fill_in "description", with: "the blink tag has a speed attribute"
+    attach_file "file #1", rails.root.join("spec/fixtures/speed.png")
+    attach_file "file #2", rails.root.join("spec/fixtures/speed.png")
+    attach_file "file #3", rails.root.join("spec/fixtures/speed.png")
+    click_button "create ticket"
+
+  end
+
   
   before do
     define_permission!(user, "view", project)
