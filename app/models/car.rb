8378df4133026e36d26ede2db533d3172ba552af
Jesus Romero <jose.romero@tangosource.com>
2013-09-11 15:38:50 -0500

    test

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,12 +1,25 @@
 class Car < ActiveRecord::Base
+  attr_accessor :tag_names
+  #attr_accessible :tag_names
+  
+ 
+
   belongs_to :state
   belongs_to :user
+
   has_many :assets
+  has_many :comments
   has_and_belongs_to_many :tags
   #has_attached_file 
   accepts_nested_attributes_for :assets
-  has_many :comments
-
-
-
+  before_create :associate_tags
+  private 
+  def associate_tags
+    if tag_names
+      #binding.pry
+      tag_names.split(" ").each do |name|
+        self.tags << Tag.find_or_create_by_name(name)
+      end
+    end
+  end
 end
--- db/migrate/20130911174409_create_tags.rb
+++ db/migrate/20130911174409_create_tags.rb
@@ -3,5 +3,9 @@ class CreateTags < ActiveRecord::Migration
     create_table :tags do |t|
       t.string :name
     end
+
+    create_table :tags_cars, :id => false do |t|
+      t.integer :tag_id, :car_id
+    end
   end
 end
