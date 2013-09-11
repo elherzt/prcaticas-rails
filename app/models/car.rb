fc81f8bbf54c80a9280ab7f1a960c4f03e724524
Jesus Romero <jose.romero@tangosource.com>
2013-09-11 12:44:38 -0500

    created Tag model

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,7 @@ class Car < ActiveRecord::Base
   belongs_to :state
   belongs_to :user
   has_many :assets
+  has_and_belongs_to_many :tags
   #has_attached_file 
   accepts_nested_attributes_for :assets
   has_many :comments
