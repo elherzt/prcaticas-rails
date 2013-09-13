5e7f41db910ceedbd7767f240c1fcff1bb887bb7
Jesus Romero <jose.romero@tangosource.com>
2013-09-13 11:44:41 -0500

    refactor method index at v1/users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::UsersController < Api::V1::BaseController
+  #def index
+    #respond_with(User.all)
+  #end
   def index
-    respond_with(User.all)
+    respond_with(User.for(current_user).all)
   end
 end
