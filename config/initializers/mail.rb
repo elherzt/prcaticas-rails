44779887560d19118fe3b0696417f4c2aaf5a11f
Jesus Romero <jose.romero@tangosource.com>
2013-09-19 19:14:11 -0500

    test

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :user_name => 'jose.romero@tangosource.com',
   :address => "smtp.gmail.com",
-  :password => 'examplexD',
+  :password => '...jv1702',
   :port => 587,
   :enable_starttls_auto => true
 }
